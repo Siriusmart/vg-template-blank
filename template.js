db65d2f6c7233c93f51e74311fb78f918f056707
siriusmart <siriusmart@tuta.io>
2025-08-23 15:45:13 +0100

    bump

--- template.js
+++ template.js
@@ -3,74 +3,100 @@ const yaml = require("js-yaml");
 class BlankTemplate {
     constructor() {}
 
-    postProcess(dom) {
-        for (let [name, { listener, params }] of Object.entries(
-            this.components ?? {},
-        )) {
-            dom.window.document
-                .querySelectorAll(`pre code.language-${name}`)
-                .forEach((elem) => {
-                    let inSettings = params.hasSettings ?? true;
-                    let settingsLines = [];
-                    let contentLines = [];
+    async toHTML(_markdown, _resources) {}
 
-                    for (let line of elem.innerHTML.split("\n")) {
-                        if (inSettings) {
-                            if (line == "---") {
-                                inSettings = false;
-                                continue;
-                            }
+    postProcess(postProcParams) {
+        this.redo = false;
 
-                            settingsLines.push(line);
-                        } else {
-                            contentLines.push(line);
-                        }
-                    }
+        let dom = postProcParams.dom;
 
-                    let settings = yaml.load(settingsLines.join("\n")) ?? {};
-                    let content = contentLines.join("\n");
+        dom.window.document
+            .querySelectorAll(`pre code[class^="language-"]`)
+            .forEach((elem) => {
+                elem.classList.forEach((className) => {
+                    if (!className.startsWith("language-")) return;
+                    let name = className.slice(9);
 
-                    let producedElem = listener({
-                        content,
-                        options: settings,
-                        elem,
-                        dom,
-                        window: dom.window,
-                        document: dom.window.document,
-                    });
+                    for (let { listener, params } of this.components[name] ??
+                        []) {
+                        dom.window.document
+                            .querySelectorAll(`pre code.language-${name}`)
+                            .forEach((elem) => {
+                                let inSettings =
+                                    params.hasSettings ??
+                                    ((params.settingsOptional ?? true)
+                                        ? elem.innerHTML
+                                              .split("\n")
+                                              .includes("---")
+                                        : true);
 
-                    if (typeof producedElem == "string") {
-                        elem.parentNode.outerHTML = producedElem;
-                    } else if (producedElem != undefined) {
-                        if (!Array.isArray(producedElem)) {
-                            producedElem = [producedElem];
-                        }
+                                let settingsLines = [];
+                                let contentLines = [];
 
-                        elem.parentNode.outerHTML = producedElem
-                            .flat(Infinity)
-                            .map((elem) =>
-                                typeof elem == "string" ? elem : elem.outerHTML,
-                            )
-                            .join("");
+                                for (let line of elem.innerHTML.split("\n")) {
+                                    if (inSettings) {
+                                        if (line == "---") {
+                                            inSettings = false;
+                                            continue;
+                                        }
+
+                                        settingsLines.push(line);
+                                    } else {
+                                        contentLines.push(line);
+                                    }
+                                }
+
+                                let settings =
+                                    yaml.load(settingsLines.join("\n")) ?? {};
+                                let content = contentLines.join("\n");
+
+                                let producedElem = listener({
+                                    ...postProcParams,
+                                    content,
+                                    options: settings,
+                                    elem,
+                                    dom,
+                                    window: dom.window,
+                                    document: dom.window.document,
+                                });
+
+                                if (producedElem != undefined) {
+                                    if (!Array.isArray(producedElem)) {
+                                        producedElem = [producedElem];
+                                    }
+
+                                    elem.parentNode.outerHTML = producedElem
+                                        .flat(Infinity)
+                                        .map((elem) =>
+                                            typeof elem == "string"
+                                                ? elem
+                                                : elem.outerHTML == undefined
+                                                  ? elem
+                                                  : elem.outerHTML,
+                                        )
+                                        .join("");
+                                }
+                            });
                     }
                 });
-        }
+            });
 
         for (let [query, { listener, params }] of Object.entries(
             this.mappers ?? {},
         )) {
             dom.window.document.querySelectorAll(query).forEach((elem) =>
-                listener(
-                    {
-                        elem,
-                        dom,
-                        window: dom.window,
-                        document: dom.window.document,
-                    },
-                    params,
-                ),
+                listener({
+                    ...postProcParams,
+                    ...params,
+                    elem,
+                    dom,
+                    window: dom.window,
+                    document: dom.window.document,
+                }),
             );
         }
+
+        return this.redo;
     }
 
     registerMapper(query, listener, params = {}) {
@@ -80,7 +106,8 @@ class BlankTemplate {
 
     registerComponent(name, listener, params = {}) {
         this.components ??= {};
-        this.components[name] = { listener, params };
+        this.components[name] ??= [];
+        this.components[name].push({ listener, params });
     }
 }
 
